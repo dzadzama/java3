ec5153a87d9f0ad0b8beb268c94aa8b06b32f829
dzadzama <106168094+dzadzama@users.noreply.github.com>
2022-11-22 16:19:01 +0400

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,115 @@
+ 1.
+
+ //let numbers = [5, 25, 89, 120, 36];
+ //numbers.push("javascript", "python");
+ //numbers.unshift("html", "css");
+ //console.log(numbers);
+ //console.log(numbers.length);
+ //numbers.pop();
+ //numbers.shift();
+ //console.log(numbers);
+
+ 2.
+// let array = ['ფორთოხალი', 'ბანანი', 'მსხალი'];
+// console.log(array.length);
+// array.splice(3,0, 'ვაშლი', 'ანანასი');
+// array.splice(0,0, 'საზამთრო');
+// console.log(array);
+// console.log(array.length);
+// array.splice(3,0, 'მანგო');
+// console.log(array);
+// array.splice(0,1);
+// array.splice(7,0);
+// console.log(array.length);
+
+
+ 3.
+
+ // let array = [12, 25, 3, 6, 8, 14, 7, 23];
+// let newArray = array.map((item) => item/3);
+// console.log(newArray);
+
+4.
+
+ //let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+ //let NumberArray = array.filter(function(item){
+  //  return item >= 0;
+ //})
+// console.log(NumberArray);
+
+5.
+
+// let languges=["html", "css", "javascripts", "python", "php"];
+// let newlanguges=languges.filter((item)=>item.length>3)
+// console.log(newlanguges);
+
+6.
+
+
+
+7.
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+
+// let newWords = words.filter(function(item){
+//     if(item.includes('m') ||
+//       item.includes('M'))
+//     {
+//         return true;
+//     }
+//     return false;
+// })
+// console.log(newWords);
+
+8.
+
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+// let union = arr1.concat(arr2,arr3);
+// console.log(union);
+
+9.
+
+// let arr=[1,2,3,4,5];
+// let sum=0;
+// arr.forEach(function(element){
+//     sum +=element;
+// })
+// console.log(sum);
+
+
+10.
+
+// let numbers = [-2, 5, 10, -5, -1, 1, 3, 4, -15];
+
+// let positive = numbers.filter(function(item){
+//    if(item > 0){
+//     return true;
+//    }
+//    return false;
+// })
+// console.log(positive);
+
+
+11.
+
+// let arr = [-1, -2, -3, 4].some(function(item){
+//     if(item > 0){
+//         return true;
+//     }
+//     return false;
+// })
+// console.log(arr);
+
+12.
+
+// let array =[23,45,32,5,87,7,3,98];
+// let newArray=array.sort((x,y)=>y-x)[array.length-1]
+// console.log(newArray);
+
+13.
+
+// let array =[1, 2, 3, 4, 5];
+// array.splice(3,0,"a","b","c")
+// console.log(array);
+
